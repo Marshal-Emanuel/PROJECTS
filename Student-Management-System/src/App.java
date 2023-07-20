7a3921d719fd79798fb46c7077c9dd8f91a197d6
Emanuel Marshal <97548460+Marshal-Emanuel@users.noreply.github.com>
2023-07-20 16:53:34 +0300

    Update App.java

--- Student-Management-System/src/App.java
+++ Student-Management-System/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, W  orld!");
+        System.out.println("Hello, W orld!");
     }
 }
