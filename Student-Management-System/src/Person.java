ef52b9fc62807756851d53dd3faf2610545ad926
Emanuel Marshal <97548460+Marshal-Emanuel@users.noreply.github.com>
2023-08-19 17:49:39 +0300

    Update Person.java

--- Student-Management-System/src/Person.java
+++ Student-Management-System/src/Person.java
@@ -6,7 +6,7 @@
     public Person(String name, String email, String password) {
         this.name = name;
         this.email = email;
-        this.password = password;
+        this.password = password; 
     }
 
     public String getName() {
